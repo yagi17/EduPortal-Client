0825f0d038852b22a1f9d030393500d9366aaba1
yagi17 <nafissadik785@gmail.com>
2024-06-02 01:00:25 +0600

    create firebase authentications

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routers/Routers";
+import AuthProvider from "./Authentication/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="max-w-screen-2xl mx-auto">
-      <RouterProvider router={router} />
-    </div>
+    <AuthProvider>
+      <div className="max-w-screen-2xl mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   </React.StrictMode>
 );
